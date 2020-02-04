2547a660a0dae1d564d3627bee9a468ec49a7d28
Yari Andres <yari.andres@gmail.com>
2020-02-04 13:32:33 +0100

    about page completed

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,39 @@
 import React from 'react';
 
+import Book from "../components/book"
+import Yari from '../images/new.png';
+
 import Layout from "../components/layout"
 
 const about = () => 
  (
   <Layout>
-
-      <div>
-        <h1>About</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut fugiat blanditiis quae laborum, possimus praesentium, consequuntur doloremque accusamus autem, officiis architecto quaerat repudiandae quo dolorem optio harum soluta quasi ratione!</p>
+    <section className="stories">
+      <div className="center-text margin-bottom-big">
+        <h2 className="heading--secondary">Hi...</h2>
       </div>
-      </Layout>
+
+      <div className="row">
+      <div className="story">
+        <figure className="story__shape">
+          <img src={Yari} alt="Yari" className="story__img"/>
+          <figcaption className="story__caption">
+            Yari A
+          </figcaption>
+        </figure>
+        <div className="story__text">
+          <h3 className="margin-bottom-small heading-tertiary">Yari Andres - Software Engineer</h3>
+          <p className="paragraph">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut beatae nulla ad dolore minus vel, distinctio obcaecati dolorum sit error autem similique commodi inventore id.  Consectetur adipisicing eli distinctio obcaecati dolorum sit error autem similique commodi inventore
+          </p>
+        </div>
+      </div>
+    </div>
+    </section>
+    
+    <Book/>
+  </Layout>
+
   );
 
 
