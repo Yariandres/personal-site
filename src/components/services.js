58ae8b260dd09c84e69e10b9ef4c4ce35d8691aa
Yari Andres <yari.andres@gmail.com>
2020-02-17 00:02:51 +0100

    services section text final

--- src/components/services.js
+++ src/components/services.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 const Services = () => (
 
   <section className="services">
@@ -10,20 +9,20 @@ const Services = () => (
       
       <h3 className="margin-bottom-small heading-tertiary">What I do</h3>
         <p className="feature__box--text paragraph">
-        There is an 80% chance that your business will fail according to Pareto's principal. I deliver laser focus outstanding software development solution design for success and to, increasing your conversion rate or monthly recurring revenue. 
+        There is an 80% chance that your business will fail according to Pareto's principal. I deliver laser focus outstanding development solutions focused on time saving and cost cutting strategies to everyday business processes.
         </p>
       </div>
       <div className="col-1-of-3">
         <h3 className="margin-bottom-small heading-tertiary">How I do this</h3>
         <p className="feature__box--text paragraph">
-        By delivering a product that is 100% aimed to solve main business goals and objectives. Weather it is a SaaS product or ecommerce site or local business solution the end result will not leave money on the table. 
+        I create an end-result that will help you outshine your competitors. It is aimed to solve business goals and objectives in the shortest time possible. Weather it is a SaaS product or ecommerce site or local business solution the end-result will not leave money on the table. 
         </p>
       </div>
 
       <div className="col-1-of-3">
         <h3 className="margin-bottom-small heading-tertiary">What Makes Me Different</h3>
         <p className="feature__box--text paragraph">
-        I don’t just build “pretty websites” or rely on guess work. Using a proven design / development system to do the selling for you I leverage from a powerful positioning and a clear value proposition to convert more visitors maximizing your revenue
+        I don’t just build “pretty websites” or rely on guess work. I use a proven design formula that does selling for you. A development strategy with the sole purpose for conversions, which means more potential for profit, which means inevitable success.
           </p>
       </div>
     </div>
